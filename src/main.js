f93ce6570ced659739557f06f900f754267953db
Chamy <yd95069506@naver.com>
2021-07-17 11:50:09 +0900

    :wrench: jest 테스트 환경 구축

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Koa from 'koa';
 
 const app = new Koa();
+
 app.use((ctx) => {
   ctx.body = 'hello world';
 });
 
-app.listen(4000, () => {
+const server = app.listen(4000, () => {
   console.log('Listening to port 4000');
 });
+
+export default server;
