0efb5ba3625ea5a32b636ae6364fdb5ab8960ea0
Chamy <yd95069506@naver.com>
2021-07-20 18:23:16 +0900

    :rotating_light: 로그인 테스트 케이스 추가

--- src/app.test.js
+++ src/app.test.js
@@ -54,3 +54,35 @@ describe('회원가입 POST /api/auth/register', () => {
     expect(response.status).toBe(400);
   });
 });
+
+describe('로그인 POST /api/auth/login', () => {
+  test('로그인 성공', async () => {
+    const email = 'test@gmail.com';
+    const password = '123456';
+    const response = await request(server).post('/api/auth/login').send({ email, password });
+    expect(response.status).toBe(200);
+    expect(response.body.username).toBe('양채훈');
+    expect(response.body.email).toBe(email);
+  });
+
+  test('잘못된 이메일', async () => {
+    const email = '123456';
+    const password = '123456';
+    const response = await request(server).post('/api/auth/login').send({ email, password });
+    expect(response.status).toBe(401);
+  });
+
+  test('잘못된 비밀번호', async () => {
+    const email = 'test@gmail.com';
+    const password = '123456789';
+    const response = await request(server).post('/api/auth/login').send({ email, password });
+    expect(response.status).toBe(401);
+  });
+
+  test('비어있는 폼 전송', async () => {
+    const email = '';
+    const password = '';
+    const response = await request(server).post('/api/auth/login').send({ email, password });
+    expect(response.status).toBe(401);
+  });
+});
