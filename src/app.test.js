b03614981a313a33ef0f067648c6f221a7671002
Chamy <yd95069506@naver.com>
2021-07-20 21:55:38 +0900

    :rotating_light: 로그인 체크, 로그아웃 테스트 케이스 추가

--- src/app.test.js
+++ src/app.test.js
@@ -3,25 +3,18 @@ import server from './main';
 import { test } from '@jest/globals';
 import User from './models/users';
 
+const EMAIL = 'test@gmail.com';
+const USERNAME = '양채훈';
+const PASSWORD = '123456';
+
 beforeAll(async () => {
   console.log('Jest starting!');
+  await request(server).get('/');
 });
 
 afterAll(() => {
   server.close();
   console.log('server closed!');
-});
-
-test('진입 테스트', async () => {
-  const response = await request(server).get('/');
-  expect(response.status).toBe(200);
-  expect(response.text).toBe('hello world');
-});
-
-test('라우터 테스트', async () => {
-  const response = await request(server).get('/api/test');
-  expect(response.status).toBe(200);
-  expect(response.text).toBe('테스트');
 });
 
 describe('회원가입 POST /api/auth/register', () => {
@@ -43,46 +36,65 @@ describe('회원가입 POST /api/auth/register', () => {
   test('회원가입 중복', async () => {
     const response = await await request(server)
       .post('/api/auth/register')
-      .send({ username: '양채훈', email: 'test@gmail.com', password: '123456' });
+      .send({ username: USERNAME, email: EMAIL, password: PASSWORD });
     expect(response.status).toBe(409);
   });
 
   test('회원가입 입력 항복 빼먹음', async () => {
-    const response = await await request(server)
-      .post('/api/auth/register')
-      .send({ username: '양채훈', email: 'test@gmail.com' });
+    const response = await await request(server).post('/api/auth/register').send({ username: USERNAME, email: EMAIL });
     expect(response.status).toBe(400);
   });
 });
 
 describe('로그인 POST /api/auth/login', () => {
   test('로그인 성공', async () => {
-    const email = 'test@gmail.com';
-    const password = '123456';
-    const response = await request(server).post('/api/auth/login').send({ email, password });
+    const response = await request(server).post('/api/auth/login').send({ email: EMAIL, password: PASSWORD });
     expect(response.status).toBe(200);
-    expect(response.body.username).toBe('양채훈');
-    expect(response.body.email).toBe(email);
+    expect(response.body.username).toBe(USERNAME);
+    expect(response.body.email).toBe(EMAIL);
   });
 
   test('잘못된 이메일', async () => {
-    const email = '123456';
-    const password = '123456';
-    const response = await request(server).post('/api/auth/login').send({ email, password });
+    const wrongEmail = '123456';
+    const response = await request(server).post('/api/auth/login').send({ email: wrongEmail, password: PASSWORD });
     expect(response.status).toBe(401);
   });
 
   test('잘못된 비밀번호', async () => {
-    const email = 'test@gmail.com';
-    const password = '123456789';
-    const response = await request(server).post('/api/auth/login').send({ email, password });
+    const wrongPassword = '123456789';
+    const response = await request(server).post('/api/auth/login').send({ email: EMAIL, password: wrongPassword });
     expect(response.status).toBe(401);
   });
 
   test('비어있는 폼 전송', async () => {
-    const email = '';
-    const password = '';
-    const response = await request(server).post('/api/auth/login').send({ email, password });
+    const emptyEmail = '';
+    const emptyPassword = '';
+    const response = await request(server).post('/api/auth/login').send({ email: emptyEmail, password: emptyPassword });
     expect(response.status).toBe(401);
+  });
+});
+
+describe('로그인 체크 GET /api/auth/check', () => {
+  test('비로그인 상태', async () => {
+    const response = await request(server).get('/api/auth/check');
+    expect(response.status).toBe(401);
+  });
+
+  test('로그인 상태', async () => {
+    const loginResponse = await request(server).post('/api/auth/login').send({ email: EMAIL, password: PASSWORD });
+    const cookie = loginResponse.headers['set-cookie'];
+    const response = await request(server).get('/api/auth/check').set('Cookie', cookie);
+    expect(response.status).toBe(200);
+    expect(response.body.username).toBe(USERNAME);
+    expect(response.body.email).toBe(EMAIL);
+  });
+});
+
+describe('로그아웃 POST /api/auth/logout', () => {
+  test('로그아웃', async () => {
+    const loginResponse = await request(server).post('/api/auth/login').send({ email: EMAIL, password: PASSWORD });
+    const cookie = loginResponse.headers['set-cookie'];
+    const response = await request(server).post('/api/auth/logout').set('Cookie', cookie);
+    expect(response.status).toBe(204);
   });
 });
