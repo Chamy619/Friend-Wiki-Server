09f396b2002d9bef60cee83088a55474e9df38ba
Chamy <yd95069506@naver.com>
2021-08-19 20:41:16 +0900

    :sparkles: 포스트 데이터 몽고 DB 에서 관리

---
+++ src/api/posts/posts.ctrl.js
@@ -0,0 +1,111 @@
+import Post from '../../models/post';
+
+/**
+ * 포스트 작성
+ * POST /api/posts
+ * @param {title, body}: {string, string} 형태가 request.body 에 존재해야 함.
+ */
+export const write = async (ctx) => {
+  const { title, body } = ctx.request.body;
+  const post = new Post({ title, body });
+
+  try {
+    await post.save();
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+/**
+ * 포스트 목록 조회
+ * GET /api/posts
+ * @param {}
+ */
+export const list = async (ctx) => {
+  try {
+    const posts = await Post.find().exec();
+    ctx.body = posts;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+/**
+ * 특정 포스트 조회
+ * GET /api/posts/:id
+ * @param {}
+ */
+export const read = async (ctx) => {
+  const { id } = ctx.params;
+
+  try {
+    const post = await Post.findById(id).exec();
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+/**
+ * 특정 포스트 제거
+ * DELETE /api/posts/:id
+ * @param {}
+ */
+export const remove = async (ctx) => {
+  const { id } = ctx.params;
+
+  try {
+    await Post.findByIdAndRemove(id).exec();
+    ctx.status = 204;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+// /**
+//  * 포스트 수정(교체)
+//  * PUT /api/posts/:id
+//  * @param {title, body}: {string, string} 형태가 request.body 에 존재해야 함
+//  */
+// export const replace = (ctx) => {
+//   const { id } = ctx.params;
+//   const index = posts.findeIndex((post) => post.id.toString() === id);
+
+//   if (index === -1) {
+//     ctx.status = 404;
+//     ctx.body = {
+//       message: '포스트가 존재하지 않습니다.',
+//     };
+//     return;
+//   }
+
+//   posts[index] = {
+//     id,
+//     title: ctx.request.body.title,
+//     body: ctx.request.body.body,
+//   };
+//   ctx.body = posts[index];
+// };
+
+/**
+ * 포스트 수정(일부분)
+ * PATCH /api/posts/:id
+ * @param {title, body}: {string, string} 형태가 request.body 안에 존재해야 하는데, 둘 중 하나만 존재해도 됨.
+ */
+export const update = async (ctx) => {
+  const { id } = ctx.params;
+
+  try {
+    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+      new: true,
+    }).exec();
+    ctx.body = post;
+  } catch (e) {
+    throw (500, e);
+  }
+};
