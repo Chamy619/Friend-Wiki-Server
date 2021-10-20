3c69387219386c09318036889e96afee452fceee
Chamy <yd95069506@naver.com>
2021-10-20 14:48:59 +0900

    :hammer: mongoose 객체 id 유효성 검증 함수를 외부 모듈로 작성

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -1,15 +1,16 @@
 import Router from 'koa-router';
 
 import checkLoggedIn from '../../lib/checkLoggedIn';
+import checkObjectId from '../../lib/checkObjectId';
 import * as postsCtrl from './posts.ctrl';
 
 const posts = new Router();
 
 posts.get('/', checkLoggedIn, postsCtrl.list);
 posts.post('/', checkLoggedIn, postsCtrl.write);
-posts.get('/:id', checkLoggedIn, postsCtrl.checkObjectId, postsCtrl.read);
-posts.delete('/:id', checkLoggedIn, postsCtrl.checkObjectId, postsCtrl.remove);
+posts.get('/:id', checkLoggedIn, checkObjectId, postsCtrl.read);
+posts.delete('/:id', checkLoggedIn, checkObjectId, postsCtrl.remove);
 // posts.put('/:id', postsCtrl.replace);
-posts.patch('/:id', checkLoggedIn, postsCtrl.checkObjectId, postsCtrl.update);
+posts.patch('/:id', checkLoggedIn, checkObjectId, postsCtrl.update);
 
 export default posts;
